fc90eedc1c4589a7533aa088367499fdf9df2da1
ERICASTPOS <ericb.astpos@gmail.com>
2020-10-30 13:48:16 -0700

    added concurrently and npm run dev to run react and server at the same time

---
+++ build/precache-manifest.36d404d1fe07d8b9aa0695e00c6b97a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5353c018628f72de2acf4f22d221e1e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69d64ed3c6545e0bb4f2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1761caf68a5eb7955ba6",
+    "url": "/static/js/2.1c608e4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1c608e4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d64ed3c6545e0bb4f2",
+    "url": "/static/js/main.5e79bb0a.chunk.js"
+  },
+  {
+    "revision": "2ebfa9f0e6c6f44ce294",
+    "url": "/static/js/runtime-main.0c480476.js"
+  }
+]);
